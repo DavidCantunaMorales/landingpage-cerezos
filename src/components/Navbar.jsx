061cab21c1bc190d7b9bfbbced4412e49e081219
SebastianTorres12 <105263296+SebastianTorres12@users.noreply.github.com>
2024-09-27 14:51:26 -0500

    Mejores movil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,16 @@ export const Navbar = () => {
 
   return (
     <>
-      <header className="flex justify-between items-center w-full p-4 h-[10vh] bg-cerezo-primary xl:justify-center">
+      <header className="flex justify-between items-center w-full p-4 h-[10vh] bg-[#ac2454] xl:justify-center">
         <div className="text-center xl:w-1/6">
-          <h1 className="text-2xl font-bold p-1 text-cerezo-secondary">
+          <h1 className="text-2xl font-bold p-1 text-white">
             Los Cerezos
           </h1>
         </div>
         <nav
           className={`fixed bg-cerezo w-[80%] h-full font-bold ${
-            showMenu ? "left-0 bg-cerezo-primary" : "-left-full"
-          }  top-0 flex-1 flex flex-col items-center justify-center gap-10 transition-all duration-500 ease-in-out text-cerezo-secondary
+            showMenu ? "left-0 bg-[#ac2454]" : "-left-full"
+          }  top-0 flex-1 flex flex-col items-center justify-center gap-10 transition-all duration-500 ease-in-out text-white
             xl:static xl:flex-row xl:w-full`}
         >
           <a href="/">Inicio</a>
@@ -32,7 +32,7 @@ export const Navbar = () => {
           className="text-2xl xl:hidden"
           onClick={() => setShowMenu(!showMenu)}
         >
-          {showMenu ? <IoIosClose /> : <IoIosMenu />}
+          {showMenu ? <IoIosClose className="text-white" /> : <IoIosMenu className="text-white"/>}
         </button>
       </header>
       <Outlet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,43 +3,40 @@ import CerezosImage from "../assets/cerezos-home.webp";
 export const Home = () => {
   return (
     <>
-      { <section className="h-[90vh] grid grid-cols-1 md:grid-cols-8">
+      { 
+      <section className="h-[90vh] grid grid-cols-1 md:grid-cols-8 bg-gray-100">
         <div className="md:col-span-5 flex items-center justify-center p-8">
           <div className="flex flex-col gap-4">
-            <h1 className="text-7xl font-bold text-center">
-              Bienvenidos a La Residencia <span>Los Cerezos</span>
+            <h1 className="text-4xl md:text-7xl font-bold text-center">
+              Bienvenidos a la Residencia <br /><span>Los Cerezos</span>
             </h1>
-            <p className="text-gray-500 text-xl leading-6">
+            <p className="text-gray-500 text-xl leading-6 text-justify my-5">
               En la Residencia Los Cerezos, nos comprometemos a brindar una
               atención de calidad y un ambiente cálido para nuestros residentes.
               Nos esforzamos por ofrecer una vida digna, llena de actividades
               que promuevan el bienestar físico, mental y emocional de cada
               persona.
             </p>
-            <p className="text-gray-500 text-xl leading-6">
+            <p className="text-gray-500 text-xl leading-6 text-justify">
               Contamos con un equipo de profesionales dedicados que proporciona
               cuidados personalizados, asegurando que cada residente reciba la
               atención que necesita en un entorno de respeto y cariño. Nuestro
               objetivo es que se sientan como en casa, rodeados de seguridad y
               confort.
             </p>
-            <button className="bg-red-600 text-white py-2 px-6 rounded-full hover:bg-red-700 transition duration-300">
+            <button className="bg-red-600 mt-5 text-white text-xl py-2 px-6 rounded-full hover:bg-red-700 transition duration-300">
               Conoce Más
             </button>
           </div>
         </div>
-        <div className="md:col-span-3">
-          <img
-            src={CerezosImage}
-            alt="Residencia Los Cerezos"
-            className="h-full w-full object-cover p-5"
-          />
-        </div>
-      </section> }
 
-      <section className="flex flex-col bg-gray-100 items-center gap-8 p-6 md:flex-row md:items-start md:p-12">
+      </section> 
+      
+      }
+
+      <section className="flex flex-col pt-28 bg-gray-100 items-center gap-8 p-6 md:flex-row md:items-start md:p-12">
         {/* Sección de texto */}
-        <div className="md:w-[40%] bg-red-400 md:mt-52 md:text-center">
+        <div className="md:w-[40%] md:mt-52 md:text-center">
           <h2 className="text-2xl font-bold text-gray-800 mb-4">
             ¿Por qué elegirnos?
           </h2>
